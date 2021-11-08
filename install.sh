8a3733b168b57b8fa8f3d87b922417707d24f85e
Ashpex <38400042+Ashpex@users.noreply.github.com>
2021-11-08 18:05:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-
+#!/bin/sh
+pacman -Qqe > pkglist.txt
+sudo pacman -S $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
+sudo pacman -S - < pkglist.txt
+sudo pacman -Rsu $(comm -23 <(pacman -Qq | sort) <(sort pkglist.txt))
